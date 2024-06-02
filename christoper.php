4c6b9344b6853118829efc765c35fc0a888fe041
John Meward Lanoy <149600044+wadelargo@users.noreply.github.com>
2024-06-03 00:30:48 +0800

    first commit

---
+++ christoper.php
@@ -0,0 +1,12 @@
+<?php
+    function name(){
+    $name = "Christoper Sab";
+    print "<h1>
+                    <center>
+                        $name
+                    </center>
+                </h1>";
+    }
+
+name();
+?>
---
+++ name.php
@@ -0,0 +1,12 @@
+<?php
+$year = 2;
+
+function student(){
+    $name = "I am my john meward";
+    $course = "Lanoy";
+    echo "Welcome my babe jhana $name <br>";
+    echo "nahigugma siya nako tungsod sa kagwapo HEHEHEHE";
+}
+student();
+
+?>
